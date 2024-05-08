5a8c766c75e8cf4a230737dc3ea8073491b96798
YLJOSE <joserojasr47@gmail.com>
2024-05-08 19:06:24 +0200

    actualizacion de ejercicios en intellijei con base de datos

--- src/main/java/EjerciciosSeis/EjercicioDos.java
+++ src/main/java/EjerciciosSeis/EjercicioDos.java
@@ -2,17 +2,15 @@ package EjerciciosSeis;
 
 import java.sql.*;
 
-public class EjercicioUno {
+public class EjercicioDos {
     /*
      * Username and password and login URL
      * */
     final static String url = "jdbc:mysql://localhost:3306/ej1";
     final static String userName = "root";
     final static String password = "admin";
-    final static String instSQLUpdate = "create table PersonasPais(id int primary key," +
-            " nombre varchar(25), edad tinyint, nombrePais varchar(25), tamnno varchar(25))";
-    final static String instSQLConsult = "select persona.id, persona.nombre," +
-            "persona.edad,pais.nombrePais,pais.tamañoPais from persona join pais on pais.idPais = persona.id;";
+    final static String instSQLConsult = "select e.emp_no,e.apellido,e.salario,d.dnombre " +
+            "from empleados e join departamentos d on e.dept_no=d.dept_no; ";
 
     public static void main(String[] args) {
         try {
@@ -28,11 +26,8 @@ public class EjercicioUno {
         Statement st2 = conectionJnr.createStatement();
         consultationsSQL(st, st2);
     }
-
     static void consultationsSQL(Statement st, Statement st2) throws SQLException {
-        /*
-         * create table if exists db*/
-        st.executeUpdate(instSQLUpdate);
+
         ResultSet rst = st.executeQuery(instSQLConsult);
         int id, edad;
         String nombre, nombrePais, tamnnoPais, instSQLInsert;
@@ -46,11 +41,7 @@ public class EjercicioUno {
             instSQLInsert = "insert into PersonasPais values(" + id + "," +
                     "'" + nombre + "'" + "," + edad + "," + "'" + nombrePais +
                     "'" + "," + "'" + tamnnoPais + "'" + ");";
-            insertTable(st2, instSQLInsert);
+           // insertTable(st2, instSQLInsert);
         }
-    }
-
-    static void insertTable(Statement st, String instSQLInsert) throws SQLException {
-        st.executeUpdate(instSQLInsert);
     }
 }
